5a45a155b28a14475c5cff2f064e3c81097e93cd
FyomyuaLizeg <31659436+FyomyuaLizeg@users.noreply.github.com>
2024-11-25 16:30:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import 'dotenv/config';
-import { db } from './lib/dbConnect.js';
 import userRouter from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
